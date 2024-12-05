9d342f32f2c1bb0e0a5586f9df66552a5a892631
edinabonifert <edinabonifert@gmail.com>
2024-12-05 18:05:16 +0100

    bug fix - newly added comment field was not included in the dto

--- CostRegisterAppNET8/DTOs/CostEntryDto.cs
+++ CostRegisterAppNET8/DTOs/CostEntryDto.cs
@@ -5,4 +5,5 @@ public class CostEntryDto
     public required string Category { get; set; }
     public required decimal Total { get; set; }
     public required DateTime Date { get; set; }
+    public string Comment { get; set; } = null!;
 }
